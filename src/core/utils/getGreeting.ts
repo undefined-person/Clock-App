1ebdb1f8dc25dab6cdf7d17fed3138be84376463
Anastasia Karachova <nastiakarachova@gmail.com>
2022-10-09 12:42:09 +0300

    fix: greeting text

--- src/core/utils/getGreeting.ts
+++ src/core/utils/getGreeting.ts
@@ -1,7 +1,7 @@
 export const getGreeting = (hour: number) => {
   if (hour >= 5 && hour < 12) {
     return 'Good morning'
-  } else if (hour > 12 && hour < 18) {
+  } else if (hour >= 12 && hour < 18) {
     return 'Good afternoon'
   }
   return 'Good evening'
