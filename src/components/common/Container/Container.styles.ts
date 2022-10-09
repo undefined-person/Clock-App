712a9e159b5668ea5447437229aa3d50b4ca5544
Anastasia Karachova <nastiakarachova@gmail.com>
2022-10-09 12:51:51 +0300

    fix: add max-height for container

--- src/components/common/Container/Container.styles.ts
+++ src/components/common/Container/Container.styles.ts
@@ -4,6 +4,7 @@ export const ContainerStyles = styled.div<{
   image: string
 }>`
   height: 100vh;
+  max-height: -webkit-fill-available;
   position: relative;
   background-image: ${(props) => `url(${props.image})`};
   background-repeat: no-repeat;
